1211907e16795ffe17bec080c8c8372dd22c2622
Aaron Kitzmiller <aaron_kitzmiller@harvard.edu>
2017-10-19 15:54:24 -0400

    replacement for module commands; passes spider and query off to module-query while the rest go to lmod

---
+++ helmod.sh
@@ -0,0 +1,50 @@
+#
+# HeLmod bash function.
+# Wraps some of the module load functionality and provides a few other tidbits 
+# specific to HeLmod
+#
+
+join_by() { 
+    local IFS="$1"; shift; echo "$*"; 
+}
+
+helmod() {
+    case $1 in 
+        os)
+            shift;
+            if [ -z "$1" ]; then 
+                echo "$FASRCSW_OS"
+                return 0
+            else
+                export FASRCSW_OS="${1}" 
+            fi
+            unset MODULEPATH_ROOT && source lmod.sh
+            ;;
+        prod)
+            shift;
+            if [ -z "$1" ]; then
+                echo "$FASRCSW_PROD" 
+                return 0
+            else
+                export FASRCSW_PROD="${1}"
+            fi
+            unset MODULEPATH_ROOT && source lmod.sh
+            ;;
+        env)
+            shift;
+            unsets=""
+            for e in FASRCSW_PROD FASRCSW_DEV FASRCSW_OS FASRCSW_COMPS FASRCSW_MPIS FASRCSW_CUDAS NAME VERSION RELEASE TYPE; do 
+                test -z "${!e}" && unsets="$unsets $e" || echo "$e=${!e}"
+            done
+            test -n "$unsets" && echo ""; echo "Unset variables"; join_by , $unsets
+            ;;
+        spider | query)
+            shift;
+            module-query.py $@
+            ;;
+        *)
+            eval $($LMOD_CMD bash "$@") && eval $(${LMOD_SETTARG_CMD:-:} -s sh)
+            ;;
+    esac
+}
+export -f helmod
